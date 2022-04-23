df44383474370ace64e34e7b65c22d0d894876dc
Omers5 <31316779+Omers5@users.noreply.github.com>
2022-04-23 15:05:58 -0400

    Update hashTemplate.h

--- hashTemplate.h
+++ hashTemplate.h
@@ -1,5 +1,28 @@
+#include <vector>
+
+using namespace std;
+//to be used with either linked list or BST as bucket
 template <typename bucket>
 class HashTable
 {
-	
+	public:
+		addBidder(string name, int id)
+		{
+			Table[id%tableSize].add(name, id);
+		}
+		//this should be also used for assassinateRandomBidder
+		//	id for this should be sent from id that was removed from heap
+		assassinateBidder(int id)
+		{
+			Table[id%tableSize].remove(id);
+		}
+		getBidderName(int id)
+		{
+			Table[id%tableSize].getName(id);
+		}
+			
+		
+	private:
+		int tableSize;
+		vector<bucket> Table;
 };
