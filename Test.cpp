31a56097eb44c4ea2d8ba012e981f3a005ef7a48
Omers5 <31316779+Omers5@users.noreply.github.com>
2022-04-25 11:16:31 -0400

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+//this executable will take no arguments
+//  initialize all 3 options of hash table
+//  Run pre-set tests on all 3 hash tables
+//  output the time taken for each method on hash table
+
+
+#include "LinkedList.h"
+#include "hashTemplate.h"
+
+
+int main (int argc, char *argv[]) {
+  
+}
