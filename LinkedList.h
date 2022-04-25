bc1740f09a2f33081975daf5fd168559f888583f
Omers5 <31316779+Omers5@users.noreply.github.com>
2022-04-25 11:25:14 -0400

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -15,65 +15,75 @@ struct Node
 
 class LinkedList
 {
-  Node * head;
+  private:
+    Node * head;
   public:
-  LinkedList()
-  {
-    head = nullptr;
-  }
-  void add(int id, string name)
-  {
-    Node* hold = head;
-    Node* holdp = head;
-    while(hold != nullptr)
+    LinkedList()
     {
-      if(id == hold.id)
-      {
-        //?? what to do about this?
-      }
-      holdp = hold;
-      hold = hold.next;
+      head = nullptr;
     }
-    Node holdin = new Node();
-    holdin.id = id;
-    holdin.name = name;
-    holdin.next = nullptr;
-    holdp.next = holdin;
-  }
-  //return name
-  string remove(int id)
-  {
-    Node* hold = head;
-    Node* holdp = head;
-    while(hold != nullptr)
+    ~LinkedList()
     {
-      if(id == hold.id)
+      while(head != nullptr)
       {
-        holdp.next = hold.next;
-        string nout = hold.name;
-        delete out;
-        return nout;
+        Node * hold = head;
+        head = head->next;
+        delete hold;
       }
-      holdp = hold;
-      hold = hold.next;
     }
-    return "-1";
-  }
-  string getName(int id)
-  {
-    Node* hold = head;
-    Node* holdp = head;
-    while(hold != nullptr)
+    void add(int id, string name)
     {
-      if(id == hold.id)
+      Node* hold = head;
+      Node* holdp = head;
+      while(hold != nullptr)
       {
-        return hold.name;
+        if(id == hold->id)
+        {
+          //?? what to do about this?
+        }
+        holdp = hold;
+        hold = hold->next;
       }
-      holdp = hold;
-      hold = hold.next;
+      Node* holdin = new Node();
+      holdin->id = id;
+      holdin->name = name;
+      holdin->next = nullptr;
+      holdp->next = holdin;
     }
-    return "-1";
-  }
+    //return name
+    string remove(int id)
+    {
+      Node* hold = head;
+      Node* holdp = head;
+      while(hold != nullptr)
+      {
+        if(id == hold->id)
+        {
+          holdp->next = hold->next;
+          string nout = hold->name;
+          delete out;
+          return nout;
+        }
+        holdp = hold;
+        hold = hold->next;
+      }
+      return "-1";
+    }
+    string getName(int id)
+    {
+      Node* hold = head;
+      Node* holdp = head;
+      while(hold != nullptr)
+      {
+        if(id == hold->id)
+        {
+          return hold->name;
+        }
+        holdp = hold;
+        hold = hold->next;
+      }
+      return "-1";
+    }
 }
   
 #endif
