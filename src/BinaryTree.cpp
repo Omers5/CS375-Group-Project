0e5f7081e5ba8cba1666772eef6445742d7384ff
Omers5 <31316779+Omers5@users.noreply.github.com>
2022-05-03 18:33:25 -0400

    Update BinaryTree.cpp

--- src/BinaryTree.cpp
+++ src/BinaryTree.cpp
@@ -3,282 +3,166 @@
 BinaryTree::BinaryTree()
 {
     this->headNode = nullptr;
-    this->nElemenets = 0;
 }
 
 // Add node to the binary search tree
-void BinaryTree::addNode(const User &userToAdd)
+void BinaryTree::add(int id, string name)
 {
-    BinaryTreeNode *newNode = new BinaryTreeNode(userToAdd);
-    BinaryTreeNode *currentNode = this->headNode;
+    BTNode *newNode = new BTNode();
+    newNode->name = name;
+    newNode->id = id;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    BTNode *currentNode = this->head;
     bool lookingForSpot = true;
+    if(currentNode == nullptr)
+    {
+        headNode = newNode;
+        return;
+    }
     // Check for empty tree
-    while (lookingForSpot)
+    while(currentNode != nullptr)
     {
-        if (currentNode == nullptr)
+        if(newNode->id < currentNode->id)
         {
-            headNode = newNode;
-            lookingForSpot = false;
-            ++this->nElemenets;
-        }
-
-        else
-        {
-            // Check if newNode has a lower alphabetical order than the currentNode
-            if (*(newNode->getUser()) < *(currentNode->getUser()))
+            if(currentNode->left == nullptr)
             {
-                // newNode is smaller
-                if (currentNode->getLeftChild() == nullptr)
-                {
-                    currentNode->setLeftChild(newNode);
-                    /*cout << currentNode->getUser()->getName() << " left child = "
-                         << newNode->getUser()->getName() << endl;*/
-                    lookingForSpot = false;
-                    ++this->nElemenets;
-                }
-                else
-                {
-                    currentNode = currentNode->getLeftChild();
-                }
+                currentNode->left = newNode;
+                return;
+            } else {
+                currentNode = currentNode->left;
             }
-            else
+        } else {
+            if(currentNode->right == nullptr)
             {
-                // newNode is larger
-                if (currentNode->getRightChild() == nullptr)
-                {
-                    currentNode->setRightChild(newNode);
-                    /*cout << currentNode->getUser()->getName() << " right child = "
-                         << newNode->getUser()->getName() << endl;*/
-                    lookingForSpot = false;
-                    ++this->nElemenets;
-                }
-                else
-                {
-                    currentNode = currentNode->getRightChild();
-                }
+                currentNode->right = newNode;
+                return;
+            } else {
+                currentNode = currentNode->right;
             }
         }
     }
-
-    //free(newNode);
-    //free(currentNode);
 }
 
-//Lookup username and return a pointer to the user, returns nullpointer if not present
-User *BinaryTree::lookupUser(string userName)
+// remove a node from a tree and return name, returns -1 if user is not present or tree is empty
+bool BinaryTree::remove(int id)
 {
-
-    // Make sure tree is not empty
-    if (headNode == nullptr)
-    {
-        return nullptr;
-    }
-
-    BinaryTreeNode *currentNode = headNode;
-
-    while (currentNode != nullptr)
-    {
-        if (currentNode->getUser()->getName() == userName)
-        {
-            return currentNode->getUser();
-        }
-        // Check if the userName is more or less than currentNode, alphabetically
-        if (userName < currentNode->getUser()->getName())
-        {
-            // userName is smaller
-            currentNode = currentNode->getLeftChild();
-        }
-        else
-        {
-            currentNode = currentNode->getRightChild();
-        }
-    }
-    return nullptr;
-}
-
-
-
-//Lookup username and return a pointer to the BinaryTreeNode, returns nullpointer if not present
-BinaryTreeNode *BinaryTree::lookupNode(string userName)
-{
-
-    // Make sure tree is not empty
-    if (headNode == nullptr)
-    {
-        return nullptr;
-    }
-
-    BinaryTreeNode *currentNode = headNode;
-
-    while (currentNode != nullptr)
-    {
-        if (currentNode->getUser()->getName() == userName)
-        {
-            return currentNode;
-        }
-        // Check if the userName is more or less than currentNode, alphabetically
-        if (userName < currentNode->getUser()->getName())
-        {
-            // userName is smaller
-            currentNode = currentNode->getLeftChild();
-        }
-        else
-        {
-            currentNode = currentNode->getRightChild();
-        }
-    }
-    return nullptr;
-}
-
-// remove a node from a tree, returns false if user is not present or tree is empty
-bool BinaryTree::removeNode(string userName)
-{
-    BinaryTreeNode *currNode = this->headNode;
-    BinaryTreeNode *prevNode = this->headNode;
+    BTNode *currNode = this->head;
+    BTNode *prevNode = this->head;
     int comingFromDirection = 0; //-1 for left, 1 for right; shows the direction the the currentNode came from
-
-    // Check if tree is empty
-    if (headNode == nullptr)
-    {
-        return false;
-    }
-
-    // Check if the Head is the only node
-    if (currNode == headNode && headNode->getUser()->getName() == userName)
-    {
-        if (currNode->getLeftChild() == nullptr && currNode->getRightChild() == nullptr)
-        {
-            this->headNode = nullptr;
-            // delete headNode;
-            --this->nElemenets;
-            // cout<<"ONLY HEADNODE\n";
-            // headNode->deleteData(1);
-            return true;
-        }
-    }
-
-    // Move current node to the next value
-    /*
-    if (currNode->getUser() < tempUser)
-    {
-        currNode = currNode->getRightChild();
-        comingFromDirection = 1;
-    }
-    else
-    {
-        currNode->getLeftChild();
-        comingFromDirection = -1;
-    }
-    */
 
     while (currNode != nullptr)
     {
         
-        if (currNode->getUser()->getName() == userName)
+        if (currNode->id == id)
         {
+            string nout = currNode->name;
         
             // Check if leaf node
-            if (currNode->getRightChild() == nullptr && currNode->getLeftChild() == nullptr)
+            if (currNode->right == nullptr && currNode->left == nullptr)
             { 
-                // if(prevNode==nullptr)
-                // {
-                //     cout<<"\nPREV NODE IS NULL=======================\n";
-                // }
                 if (comingFromDirection > 0)
                 {
-                    prevNode->setRightChild(nullptr);
+                    prevNode->right = nullptr;
                 }
-                else
+                else if(comingFromDirection < 0)
                 {
-                    prevNode->setLeftChild(nullptr);
+                    prevNode->left = nullptr;
+                } else if(currNode == head) {
+                    head = nullptr;
                 }
             }
 
             // Case if left is null, right is not
-            else if (currNode->getLeftChild() == nullptr && currNode->getRightChild() != nullptr)
+            else if (currNode->left == nullptr && currNode->right != nullptr)
             {
                 if (comingFromDirection > 0)
                 {
-                    prevNode->setRightChild(currNode->getRightChild());
+                    prevNode->right = currNode->right;
                 }
-                else
+                else if(comingFromDirection < 0)
                 {
-                    prevNode->setLeftChild(currNode->getRightChild());
+                    prevNode->left = currNode->right;
+                } else if(currNode == head) {
+                    head = currNode->right;
                 }
             }
             // Case if right is null, left is valid
-            else if (currNode->getLeftChild() != nullptr && currNode->getRightChild() == nullptr)
+            else if (currNode->left != nullptr && currNode->right == nullptr)
             {
                 if (comingFromDirection > 0)
                 {
-                    prevNode->setRightChild(currNode->getLeftChild());
+                    prevNode->right = currNode->left;
                 }
-                else
+                else if(comingFromDirection < 0)
                 {
-                    prevNode->setLeftChild(currNode->getLeftChild());
+                    prevNode->left = currNode->left;
+                } else if(currNode == head) {
+                    head = currNode->left;
                 }
             }
 
             // If both the right and left are valid
             else if (currNode->getLeftChild() != nullptr && currNode->getRightChild() != nullptr)
             {
-                User *minUser = findMinUser(currNode);
+                BTNode * nhold = currNode->left;
+                BTNode * nholdp = currNode;
+                while(nhold->left != nullptr)
+                {
+                    nholdp = nhold;
+                    nhold = nhold->left;
+                }
+                nholdp->left = nhold->right;
+                currNode->name = nhold->name;
+                currNode->id = nhold->id;
                 // cout << minUser->getName() << endl;
-                currNode->setUser(*minUser);
+                currNode = nhold;
             }
-            --this->nElemenets;
-
-            return true;
+            delete currNode
+            return nout;
         }
 
         // Move up prev and current Value
         prevNode = currNode; // Move prev node to the current node
-        if (currNode->getUser()->getName() < userName)
+        if (currNode->id < id)
         {
-            currNode = currNode->getRightChild();
+            currNode = currNode->right;
             comingFromDirection = 1;
         }
         else
         {
-            currNode = currNode->getLeftChild();
+            currNode = currNode->left;
             comingFromDirection = -1;
         }
     }
     
-    return false;
+    return "-1";
+}
+
+string BinaryTree::getName(int id)
+{
+    BTNode* hold = head;
+    while(hold != nullptr)
+    {
+        if(hold->id < id)
+        {
+            hold = hold->right;
+        } else if(hold->id > id) {
+            hold = hold->left;
+        } else {
+            return hold->name;
+        }
+    }
+    return "-1";
 }
 
 //Print Tree with additional info
-void BinaryTree::printInfo()
+/*void BinaryTree::printInfo()
 {
     cout << "Number of Elements in tree: " << this->nElemenets << endl;
     inOrderPrint(this->headNode);
 }
 
-// Get the minimum node from a node position and removes it from the prev Node
-// ONLY USE AS A HELPER TO THE REMOVE FUNCTION; gets the inorder smallest node
-User *BinaryTree::findMinUser(BinaryTreeNode *root)
-{
-    BinaryTreeNode *currNode = root;
-    BinaryTreeNode *prevNode = root;
-    if (currNode->getRightChild() != nullptr)
-    {
-        currNode = currNode->getRightChild();
-    }
-    else
-        return nullptr;
-
-    while (currNode->getLeftChild() != nullptr)
-    {
-        prevNode = currNode;
-        currNode = currNode->getLeftChild();
-    }
-    User *tempUser = currNode->getUser();
-    ++nElemenets; // Compensates for the remove node being called
-    removeNode(currNode->getUser()->getName());
-    // prevNode->setLeftChild(nullptr);
-    return tempUser;
-}
 
 //Print Tree using in-order print
 void BinaryTree::inOrderPrint(BinaryTreeNode *root)
@@ -292,4 +176,4 @@ void BinaryTree::inOrderPrint(BinaryTreeNode *root)
     inOrderPrint(root->getLeftChild());
     cout << root->getUser()->getName() << endl;
     inOrderPrint(root->getRightChild());
-}
+}*/
