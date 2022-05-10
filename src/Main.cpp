8eacb10812b32273e7f2de863532645354502f30
Omers5 <31316779+Omers5@users.noreply.github.com>
2022-05-09 20:24:58 -0400

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -94,15 +94,16 @@ int main (int argc, char *argv[]) {
       getline(iss, idorrand, ' ');
       //assassinate here
       User userOut;
+	    cout << idorrand << endl;
       if(idorrand == "random")
       {
         userOut = maxHeap.popRandom();
       } else {
         userOut =  maxHeap.popid(stoi(idorrand));
       }
-	    //cout << userOut.name << endl;
+	    cout << userOut.name << endl;
       string nout = to_string(hashT.assassinateBidder(stoi(userOut.name)));
-	    //cout << nout << endl;
+	    cout << nout << endl;
       if(BOOLPRINTCMDS)
       	cout << "Assassinated bidder " << nout << " with id " << userOut.name << " with bid value " << userOut.amount << endl;
       //sort
